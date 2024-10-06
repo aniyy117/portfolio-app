c9a15433f966c2099ebf7ad71e4a9fc4e579e839
aniyy117 <janiket112@gmail.com>
2024-10-06 12:37:41 +0530

    Complted home section

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -23,33 +23,35 @@ const stats = [
 
 const Stats = () => {
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
-        {stats.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className="flex-1 flex gap-4 items-center justify-center"
-            >
-              <CountUp
-                end={item.num}
-                duration={5}
-                delay={2}
-                className="text-4xl xl:text-6xl font-extrabold"
-                // decimals={2}
-              />
-              <p
-                className={`${
-                  item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-                }`}
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+          {stats.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="flex-1 flex gap-4 items-center justify-center"
               >
-                {item.text}
-              </p>
-            </div>
-          );
-        })}
+                <CountUp
+                  end={item.num}
+                  duration={5}
+                  delay={2}
+                  className="text-4xl xl:text-6xl font-extrabold"
+                  // decimals={2}
+                />
+                <p
+                  className={`${
+                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  } leading-snug text-white/80`}
+                >
+                  {item.text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
