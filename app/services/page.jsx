1e5c89377669972fda8932c0e201c6bba8f4af1a
Aniket Jadhav <aniketjadhav@MacBook-Air.local>
2026-04-10 14:02:27 +0530

    fixed bugs and added missing data

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -51,23 +51,20 @@ const Services = () => {
                 className="flex-1 flex flex-col justify-center gap-6 group"
               >
                 {/* top */}
-                <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
-                    {item.num}
-                  </div>
-                  {item.href ? (
+                {item.href && (
+                  <div className="w-full flex justify-between items-center">
+                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                      {item.num}
+                    </div>
                     <Link
                       href={item.href}
                       className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex items-center justify-center hover:-rotate-45"
                     >
                       <BsArrowDownRight className="text-primary text-3xl" />
                     </Link>
-                  ) : (
-                    <div className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex items-center justify-center hover:-rotate-45">
-                      <BsArrowDownRight className="text-primary text-3xl" />
-                    </div>
-                  )}
-                </div>
+                  </div>
+                )}
+
                 {/* heading */}
                 <h2
                   className="text-[42px] font-bold leading-none text-white
